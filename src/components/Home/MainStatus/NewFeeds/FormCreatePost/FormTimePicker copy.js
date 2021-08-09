ea3623f5ecf89693b839ae7111aa1f23f49baa0a
Student-ID-Card-Identification <khoikevin2903@gmail.com>
2021-08-09 12:38:25 +0700

    update

--- src/components/Home/MainStatus/NewFeeds/FormCreatePost/FormTimePicker copy.js
+++ src/components/Home/MainStatus/NewFeeds/FormCreatePost/FormTimePicker copy.js
@@ -5,6 +5,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import {ChangePlace} from '../../../../../reducers/infoPlace';
 import {ChangePlaceName} from '../../../../../reducers/infoPlaceName';
 import moment from 'moment';
+import './FormTimePicker.css';
 
 const FormTimePicker = (props) => {
 
